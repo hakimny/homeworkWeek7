1177cdaada70a506f7f25bb66d4a36b5ee05dfe7
Hakim TAMGALT <hakim@tamgalt.com>
2020-03-07 14:30:42 -0500

    added top 10 movies to MostPopularSuperhero

--- src/com/cebd2/spark/MostPopularSuperhero.scala
+++ src/com/cebd2/spark/MostPopularSuperhero.scala
@@ -43,21 +43,38 @@ object MostPopularSuperhero {
     
     // Convert to (heroID, number of connections) RDD
     val pairings = lines.map(countCoOccurences)
-    
+
     // Combine entries that span more than one line
     val totalFriendsByCharacter = pairings.reduceByKey( (x,y) => x + y )
     
+    
+    println("##################")
+
+    
+    
     // Flip it to # of connections, hero ID
     val flipped = totalFriendsByCharacter.map( x => (x._2, x._1) )
+//    val sortedMovies = flipped.sortBy(_._1)
+//    
+//    sortedMovies.foreach(println)
     
     // Find the max # of connections
     val mostPopular = flipped.max()
-    
+    // Print out our answer!
     // Look up the name (lookup returns an array of results, so we need to access the first result with (0)).
     val mostPopularName = namesRdd.lookup(mostPopular._2)(0)
-    
-    // Print out our answer!
     println(s"$mostPopularName is the most popular superhero with ${mostPopular._1} co-appearances.") 
+    //Top 10 Movies
+    val topTenMovies = flipped.sortBy(_._1, false, 1).collect().take(10)
+     println("\n\n########## TOP 10 MOVIES ########\n\n")
+
+    for (item <- topTenMovies){
+      val name = namesRdd.lookup(item._2)(0)
+      val occurences = item._1
+      println(s"$name is the most popular superhero with $occurences co-appearances.")
+    }
+    
+  
   }
   
 }
